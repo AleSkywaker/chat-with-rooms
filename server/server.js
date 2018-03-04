3d640bbc26ce3cefc2de6bbd681105efcdeed99f
Alex Colombo <uricaine@hotmail.com>
2018-03-04 11:34:32 +0100

    emit mensaje

--- server/server.js
+++ server/server.js
@@ -7,10 +7,8 @@ const port = process.env.PORT || 4900;
 
 const publicPath = path.join(__dirname, '../public')
 
-
 var server = http.createServer(app)
 var io = socketIO(server)
-
 
 app.use(express.static(publicPath))
 
@@ -19,20 +17,17 @@ io.on('connection', (socket) => {
 
       socket.on('createMessage', (message)=>{
         console.log('mensaje', message)
-        io.emit('newMensaje',  {
-            desde : message,
-            texto : message,
-            creado : new Date().getTime()
-          }
-        )
-      })
+          io.emit('newMensaje',  {
+              desde : message.from,
+              texto : message.text,
+              creado : new Date().getTime()
+            })
+          })
 
       socket.on('disconnect', ()=>{
-        console.log(`Cliente desconectado ${socket.id}`)
+      console.log(`Cliente desconectado ${socket.id}`)
   })
 })
-
-
 
 server.listen(port, ()=>{
   console.log(`Listenning on port ${port}`)
