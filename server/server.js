c921dd27b360576fc665466e6954161a4bdb3d98
Alex Colombo <uricaine@hotmail.com>
2018-03-04 16:42:11 +0100

    añado test

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http')
 const express = require('express')
 const app = express();
 const socketIO = require('socket.io')
+const {generateMessage} = require('./utils/message');
 const port = process.env.PORT || 4900;
 
 const publicPath = path.join(__dirname, '../public')
@@ -15,13 +16,19 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {
   console.log(`New user connected ${socket.id}`)
 
+  // socket.emit from admin, text welcome to the chat app
+       socket.emit('newMensaje', generateMessage('Admin', 'Bienvenido a la APP'))
+  // socket.broadcast.emit from admin, text new user joined
+       socket.broadcast.emit('newMensaje', generateMessage('Admin', 'Un nuevo usuario se ha unido'))
+
       socket.on('createMessage', (message)=>{
-        console.log('mensaje', message)
-          io.emit('newMensaje',  {
-              desde : message.from,
-              texto : message.text,
-              creado : new Date().getTime()
-            })
+
+          io.emit('newMensaje', generateMessage(message.from, message.text))
+          // socket.broadcast.emit('newMensaje',  {
+          //     desde : message.from,
+          //     texto : message.text,
+          //     creado : new Date().getTime()
+          //   })
           })
 
       socket.on('disconnect', ()=>{
